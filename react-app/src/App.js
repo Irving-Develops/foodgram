7114151236f4375e8ce556590d9d9b95f8a43406
Irving Arreola Palacios <irving.a789@gmail.com>
2022-08-04 23:21:17 +0000

    added s to https index file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
@@ -42,6 +42,7 @@ function App() {
         </ProtectedRoute>
         <ProtectedRoute path='/' exact={true} >
           <NavBar />
+          <h1>My Home Page</h1>
         </ProtectedRoute>
         <ProtectedRoute path='/posts' exact={true} >
           <AllPosts />
--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -29,7 +29,6 @@ const NavBar = () => {
           </ul>
         </nav>
       </div>
-
     </div>
   );
 }
