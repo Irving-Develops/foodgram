b8a7a4fdaa81c9844d905e80f92e711ef194c412
Irving Arreola Palacios <irving.a789@gmail.com>
2022-08-12 22:49:31 +0000

    adding searchbar icons

--- react-app/src/components/Search/search.js
+++ react-app/src/components/Search/search.js
@@ -31,10 +31,15 @@ export default function Search() {
         }
         fetchData();
     }, []);
+
+    const closeSearch = () => {
+        setOpenSearchBar(false)
+    }
     
     return (
-        <div>
-            <label>Search</label>
+        <div className={classes.search}>
+            <svg aria-label="Search" color="#8e8e8e" fill="#8e8e8e" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeLidth="2"></path><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>
+            <svg className={classes.svg2}  style={{fill: "#c7c6c7", position: "relative", left: "260px" }} xmlns="http://www.w3.org/2000/svg" onClick={closeSearch} width="12" height="12" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"/></svg>
             <input 
                 // onClick={toggleSearch}
                 onChange={searchUsers}
@@ -58,9 +63,6 @@ export default function Search() {
                     ))}
                 </div>
             )}
-            <div>
-                testing
-            </div>
         </div>
     )
 }
